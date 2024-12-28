c2492792bf1c69fe71a442187220a33ba1f7a3e3
samafs24 <rahimafsana17@gmail.com>
2024-12-28 01:37:33 +0000

    Updates on R Shiny

--- scripts/phenotype_dashboard/v3.R
+++ scripts/phenotype_dashboard/v3.R
@@ -5,14 +5,15 @@ library(cluster)  # For clustering
 library(reshape2) # For data manipulation
 library(DBI)
 library(RMySQL)
+library(stringr)
+
 
 # Define UI
 ui <- fluidPage(
-  titlePanel("Mouse Phenotype Visualisation Dashboard"),
+  titlePanel("Statistical Analysis and Visualization of Knockout Mouses"),
   
   sidebarLayout(
     sidebarPanel(
-      textInput("gene_search", "Search Gene Symbol:", ""),
       
       # Conditional inputs for each tab
       conditionalPanel(
@@ -25,8 +26,11 @@ ui <- fluidPage(
       
       conditionalPanel(
         condition = "input.tabs == 'phenotype_mice_tab'",
+        selectInput("selected_phenotype_group", "Select Parameter Grouping:",
+                    choices = NULL, selected = NULL),
         selectInput("selected_phenotype", "Select Phenotype:",
                     choices = NULL, selected = NULL)
+      
       ),
       
       conditionalPanel(
@@ -58,7 +62,7 @@ ui <- fluidPage(
         
         tabPanel("Phenotype Scores for Knockout Mouse",
                  value = "mouse_phenotype_tab",
-                 plotOutput("mouse_phenotype_plot"),
+                 plotOutput("mouse_phenotype_plot", height = "700px"),
                  downloadButton("download_mouse_data", "Download Mouse Data")),
         
         tabPanel("Knockout Mice for Phenotype",
@@ -99,49 +103,47 @@ server <- function(input, output, session) {
   
   # Populate dropdowns
   
-  
-  observe({
-    gene_choices <- dbGetQuery(con, "SELECT DISTINCT gene_id FROM Genes;")
-    updateSelectInput(session, "selected_mouse", choices = gene_choices$gene_id)
+    observe({
+    gene_choices <- dbGetQuery(con, "SELECT DISTINCT gene_symbol FROM Genes;")
+    updateSelectInput(session, "selected_mouse", choices = gene_choices$gene_symbol)
   })
   
-  observe({
-    phenotype_choices <- dbGetQuery(con, "SELECT DISTINCT parameter_name FROM Parameters;")
-    updateSelectInput(session, "selected_phenotype", choices = phenotype_choices$parameter_name)
-  })
+    observe({
+    groups <- dbGetQuery(con, "SELECT DISTINCT group_id FROM ParameterGroupings;")
+    updateSelectInput(session, "selected_phenotype_group", choices = groups$group_id)
+    })
+    
+  
+    observe({
+      req(input$selected_phenotype_group)  # Ensure the group is selected
+      phenotype_choices <- dbGetQuery(con, sprintf("
+    SELECT DISTINCT P.parameter_name
+    FROM Parameters P
+    JOIN ParameterGroupings PG ON P.parameter_id = PG.parameter_id
+    WHERE PG.group_id = '%s';", input$selected_phenotype_group))
+      updateSelectInput(session, "selected_phenotype", choices = phenotype_choices$parameter_name)
+    })
+    
   
   observe({
     diseases <- dbGetQuery(con, "SELECT DISTINCT disease_term FROM Diseases;")
     updateSelectInput(session, "disease", choices = diseases$disease_term)
   })
+
+  # Visualisation 1: Phenotype Scores for Knockout Mouse
   
-  output$gene_select_ui <- renderUI({
-    gene_choices <- dbGetQuery(con, "SELECT DISTINCT gene_symbol FROM Genes;")
-    req(nrow(gene_choices) > 0)
-    selectInput("selected_mouse", "Select Gene:", choices = gene_choices$gene_symbol)
-  })
-  
-  # Search Gene Symbol functionality
-  observe({
-    gene_search_results <- dbGetQuery(con, paste0(
-      "SELECT DISTINCT gene_symbol FROM Genes WHERE gene_symbol LIKE '%", 
-      input$gene_search, "%'"))
-    updateSelectInput(session, "mouse_id", choices = gene_search_results$gene_symbol)
-  })
-  
-  # Visualisation 1: Phenotype Scores for a Single Knockout Mouse
   output$mouse_phenotype_plot <- renderPlot({
-    req(input$selected_mouse)
+    req(input$selected_mouse)  # Ensure a gene symbol is selected
     
     query <- sprintf("
-      SELECT A.p_value, P.parameter_name 
-      FROM Analyses A
-      JOIN Parameters P ON A.parameter_id = P.parameter_id
-      WHERE A.gene_id IN (
-        SELECT gene_id FROM Genes WHERE gene_id = '%s'
-      ) AND A.p_value <= %f
-      ORDER BY A.p_value ASC;",
-                     input$selected_mouse, input$mouse_threshold)
+    SELECT A.p_value, P.parameter_name 
+    FROM Analyses A
+    JOIN Parameters P ON A.parameter_id = P.parameter_id
+    WHERE A.gene_accession_id IN (
+      SELECT gene_accession_id FROM Genes WHERE gene_symbol = '%s'
+    ) AND A.p_value IS NOT NULL AND A.p_value > 0
+    ORDER BY A.p_value ASC;",
+                     input$selected_mouse, input$mouse_threshold)  # Use the selected gene symbol
     
     data <- dbGetQuery(con, query)
     
@@ -152,46 +154,68 @@ server <- function(input, output, session) {
     }
     
     data <- data %>%
-      mutate(color_flag = ifelse(p_value < 0.05, "Significant", "Not Significant"))
+      mutate(Threshold = ifelse(p_value < input$mouse_threshold, "Significant", "Not Significant"))
     
-    ggplot(data, aes(x = reorder(parameter_name, p_value), y = p_value, fill = color_flag)) +
-      geom_bar(stat = "identity") +
-      scale_fill_manual(values = c("Significant" = "red", "Not Significant" = "blue")) +
-      labs(title = paste("Phenotype Scores for", input$selected_mouse),
-           x = "Phenotypes", y = "p-value") +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    # Adjust the plot height
+    ggplot(data, aes(x = reorder(parameter_name, p_value), y = p_value, fill = Threshold)) +
+      geom_bar(stat = "identity", show.legend = TRUE, width = 0.7) +  # Bar plot with adjusted bar width
+      scale_fill_manual(values = c("Significant" = "palegreen3", "Not Significant" = "indianred3")) + 
+      labs(
+        title = paste("The Phenotype Scores for Knockout Mouse:", input$selected_mouse),
+        subtitle = paste("Showing phenotypes with p-value <= ", input$mouse_threshold),
+        x = "Knockout Mouse Phenotype", 
+        y = "p-value for Phenotype Association"
+      ) +
+      theme_minimal() +  # Use minimal theme for a cleaner look
+      theme(
+        axis.text.x = element_text(angle = 90, hjust = 1, face = "bold"),  
+        axis.title.x = element_text(size = 12, face = "bold"),  
+        axis.title.y = element_text(size = 12, face = "bold"),  
+        plot.title = element_text(size = 16, face = "bold", hjust = 0.5),  
+        plot.subtitle = element_text(size = 12, hjust = 0.5),  # Subtitle styling
+        axis.text.y = element_text(size = 10)  # Adjust size of y-axis labels 
+      ) +
+      geom_hline(yintercept = input$mouse_threshold, linetype = "dashed", color = "black") +  # Threshold line
+      scale_x_discrete(labels = function(x) str_wrap(x, width = 50)) # Wrap long labels to prevent overlap
   })
   
+  
   # Visualisation 2: Scores of All Knockout Mice for a Selected Phenotype
-  output$phenotype_mouse_plot <- renderPlot({
-    req(input$selected_phenotype)
+
+  output$phenotype_group_plot <- renderPlot({
+    req(input$selected_phenotype, input$selected_phenotype_group)
     
+    # Query to fetch p-values for the selected phenotype and group
     query <- sprintf("
-      SELECT A.p_value, G.gene_id 
-      FROM Analyses A
-      JOIN Parameters P ON A.parameter_id = P.parameter_id
-      JOIN Genes G ON A.gene_id = G.gene_id
-      WHERE P.parameter_name = '%s'
-      ORDER BY A.p_value ASC;",
-                     input$selected_phenotype)
+    SELECT A.p_value, G.gene_accession_id
+    FROM Analyses A
+    JOIN Parameters P ON A.parameter_id = P.parameter_id
+    JOIN Genes G ON A.gene_accession_id = G.gene_accession_id
+    JOIN ParameterGroupings PG ON P.parameter_id = PG.parameter_id
+    WHERE P.parameter_name = '%s' AND PG.group_id = '%s'
+    ORDER BY A.p_value ASC;", 
+                     input$selected_phenotype, input$selected_phenotype_group)
     
+    # Fetch data from the database
     data <- dbGetQuery(con, query)
     
+    # If no data is available, display a message
     if (nrow(data) == 0) {
       plot.new()
-      title("No data available for this phenotype.")
+      title("No data available for this selection.")
       return()
     }
     
-    data <- data %>%
-      mutate(sig_flag = ifelse(p_value < 0.05, "Significant", "Not Significant"))
-    
-    ggplot(data, aes(x = gene_id, y = p_value, color = sig_flag)) +
-      geom_point(size = 3) +
-      labs(title = paste("Scores of All Knockout Mice for Phenotype:", input$selected_phenotype),
-           x = "Knockout Mice", y = "p-value") +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    # Plotting the p-value for each gene on the x-axis
+    ggplot(data, aes(x = gene_accession_id, y = p_value)) +
+      geom_point(size = 3, color = "blue") +
+      labs(title = paste("Phenotype Data for:", input$selected_phenotype),
+           x = "Gene Accessions", y = "p-value") +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 8),
+            axis.text.y = element_text(size = 10)) +
+      theme_minimal()
   })
+  
   
   # Gene-Disease Associations Plot
   
@@ -202,7 +226,7 @@ server <- function(input, output, session) {
     query <- sprintf("
         SELECT Genes.gene_symbol, Diseases.disease_term, PhenodigmScores.phenodigm_score
         FROM PhenodigmScores
-        JOIN Genes ON PhenodigmScores.gene_id = Genes.gene_id
+        JOIN Genes ON PhenodigmScores.gene_accession_id = Genes.gene_accession_id
         JOIN Diseases ON PhenodigmScores.disease_id = Diseases.disease_id
         WHERE Genes.gene_symbol = '%s'", input$selected_mouse)
     
@@ -226,10 +250,10 @@ server <- function(input, output, session) {
   # Visualisation 3: Gene Clusters
   output$gene_cluster_plot <- renderPlot({
     query <- "
-      SELECT G.gene_id, P.parameter_name, A.p_value
+      SELECT G.gene_accession_id, P.parameter_name, A.p_value
       FROM Analyses A
       JOIN Parameters P ON A.parameter_id = P.parameter_id
-      JOIN Genes G ON A.gene_id = G.gene_id;"
+      JOIN Genes G ON A.gene_accession_id = G.gene_accession_id;"
     
     data <- dbGetQuery(con, query)
     
@@ -243,10 +267,10 @@ server <- function(input, output, session) {
       data <- data %>% filter(p_value < 0.05)
     } else if (input$gene_subset == "User-specific genes") {
       user_genes <- unlist(strsplit(input$user_genes, "\\s*,\\s*"))
-      data <- data %>% filter(gene_id %in% user_genes)
+      data <- data %>% filter(gene_accession_id %in% user_genes)
     }
     
-    cluster_data <- dcast(data, gene_id ~ parameter_name, value.var = "p_value", fill = 0)
+    cluster_data <- dcast(data, gene_accession_id ~ parameter_name, value.var = "p_value", fill = 0)
     mat <- cluster_data[, -1]
     
     if (input$cluster_method == "Hierarchical") {
